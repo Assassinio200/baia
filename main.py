e4742fe40ccdc1397ee4a6d2ac4cb2d077c08dce
Assassinio200 <46633783+Assassinio200@users.noreply.github.com>
2024-11-11 08:31:49 +0200

    Add files via upload
    acuma si random

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from collections import Counter  # https://docs.python.org/3/library/collections.html
+import random
 
-# aceasta librarie ma ajuta sa analizez ce litere sunt cele mai folosite si se actualizeaza dupa frecventa lor
-class MAnSPANZUR:#clasa la spanzaratoare
+class MAnSPANZUR:
     def __init__(self, cuvant):
         self.cuvant = cuvant
         self.ghici = set()
         self.inc = 32 #alfabetul ro are 31 de litere -inc-incercari
-
-    def lista_litere(ghicit_litere):
-        alph = set('eaocnritlsdumbăîâfgșhvțz') # ordonate dupa cele mai folosite din lista data
-        return alph - ghicit_litere
 
     def cauta_lit(self, lit):
         if lit in self.ghici:
             return False
         self.ghici.add(lit)
         if lit not in self.cuvant:
-            self.inc =self.inc- 1
+            self.inc = self.inc - 1
         return True
 
-    def terminat(self): #terminat-jocu se sfarseste
+    def terminat(self):
         return all(lit in self.ghici for lit in self.cuvant)
+
 ##################################################################clasa-hangman
 def cuv_fiser(filename):
     cuvs = {}
@@ -30,24 +27,28 @@ def cuv_fiser(filename):
             if line:
                 parts = line.split(';')
                 if len(parts) == 3:
-                    pattern, cuv = parts[1], parts[2] #despartit in 2 liste , cuvinte[2] si cele de ghicit[1]
+                    pattern, cuv = parts[1], parts[2]
                     cuvs[pattern] = cuv
     return cuvs
+
 def frecventa(lista_cuv, ghicit_litere):
     cuv_ramas = [cuv for cuv in lista_cuv.values() if not any(lit in cuv for lit in ghicit_litere)]
     lit_counts = Counter()
 
-    for cuv in cuv_ramas:#cauta cele mai folosite litere din ce a fost,chiar daca alfabetul e deja arantajat asa
+    for cuv in cuv_ramas:
         for lit in set(cuv):
             if lit not in ghicit_litere:
                 lit_counts[lit] += 1
 
-    return lit_counts.most_common() #.most_common-fct de la librarie
+    return lit_counts.most_common()
 ########################################
 def joc(lista_cuv):
     incercari_totale = 0 #counter pt litere incercate
+    items = list(lista_cuv.items())
+    random.shuffle(items) #random
+    print(items) #doar sa vad ca functineaza
 
-    for pattern, cuv in lista_cuv.items():
+    for pattern, cuv in items:
         game = MAnSPANZUR(cuv)
 
         while not game.terminat():
@@ -59,14 +60,15 @@ def joc(lista_cuv):
             incercari_totale += 1
 
     print(f"S-a realizat : {incercari_totale} de incercari")
-    print(" _______\n" 
-          "/      |\n"
+    print(" ________\n" 
+          "/       |\n"
      "|     (x_x) \n"
-     "|     \|/ \n"
-     "|      | \n"
-     "|     / \ \n"
+     "|      \|/ \n"
+     "|       | \n"
+     "|      / \ \n"
      "| \n"
-  "|______________ \n") #https://ascii.co.uk/art/hangman
+  "|______________ \n")
+
 
 ##################################################################run-u
 filename = 'cuvinte_de_verificat.txt'
